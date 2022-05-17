8648cc7b2cfdc7672f263196c0a7c788df4c37da
Nicoleta <nicoletavoinea17@yahoo.com>
2022-05-17 23:54:47 +0300

    Loading page was added, but not linked

---
+++ src/main/java/com/benchmark/benchmarkapp/pages/LoadingPage.java
@@ -0,0 +1,21 @@
+package com.benchmark.benchmarkapp.pages;
+
+import com.benchmark.benchmarkapp.Main;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadingPage{
+
+}
